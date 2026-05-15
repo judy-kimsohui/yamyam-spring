763329d8fd8ff87409a92f0acf4163373117e481
sodaakim <sohuikim2020@naver.com>
2026-05-15 14:01:54 +0900

    food csv served

--- src/main/java/com/ssafy/yamyam/model/entity/Food.java
+++ src/main/java/com/ssafy/yamyam/model/entity/Food.java
@@ -16,20 +16,33 @@ public class Food {
     @Column(name = "food_id")
     private Long foodId;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "food_type")
+    private FoodType foodType;
+
+    private String foodCode;          // 식품코드
+
     private String foodName;
     
     
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
-    
-    
-    private String referenceAmount;
-    private double energy;
-    private double protein;
-    private double fat;
-    private double carbs;
-    private double sugar;
-    private double sodium;
-    private String foodWeight;
+
+
+    private String referenceAmount; // 영양성분함량기준량 (100g)
+    private double energy;          // 칼로리(kcal)
+    private double protein;         // 단백질
+    private double fat;             // 지방
+    private double carbs;           // 탄수화물
+    private double sugar;           // 당류(g)
+    private double sodium;          // 나트륨(mg)
+    private String foodWeight;      // 식품중량(g)
+
+
+    public enum FoodType {
+        DISH,
+        PRODUCT
+    }
+
 }
---
+++ src/main/resources/scripts/test.sql
@@ -0,0 +1,8 @@
+
+SELECT *
+FROM food
+WHERE food_id = 8198;
+
+
+
+SHOW CREATE TABLE food;
