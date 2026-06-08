79c2e430feb81ed14de600699b6dc81fd91447d3
sodaakim <sohuikim2020@naver.com>
2026-06-08 16:33:53 +0900

    merge main

--- src/main/java/com/ssafy/yamyam/domain/user/controller/UserController.java
+++ src/main/java/com/ssafy/yamyam/domain/user/controller/UserController.java
@@ -11,7 +11,7 @@ import com.ssafy.yamyam.domain.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 public class UserController {
 
     private final UserService userService;
