970dacab200298d3237bbd32d606811325a1111e
jgs7784 <jgs7784@gmail.com>
2026-06-18 19:51:28 +0900

    0618

--- src/main/java/com/ssafy/yamyam/domain/video/mapper/VideoMapper.java
+++ src/main/java/com/ssafy/yamyam/domain/video/mapper/VideoMapper.java
@@ -14,8 +14,14 @@ public interface VideoMapper {
     VideoDto findById(@Param("id") Long id, @Param("userId") Long userId);
     void deleteById(@Param("id") Long id);
     void updateAnalysis(Video video);
+
+    // 내가 이 비디오를 좋아요 누를 때
     void insertLike(@Param("videoId") Long videoId, @Param("userId") Long userId);
+
+    // 내가 이 비디오 좋아요를 해제할 때
     void deleteLike(@Param("videoId") Long videoId, @Param("userId") Long userId);
+
+    // 내가 이 비디오에 좋아요를 눌렀었는가
     int existsLike(@Param("videoId") Long videoId, @Param("userId") Long userId);
     int countLikes(@Param("videoId") Long videoId);
 }
